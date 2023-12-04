22f7c889d2e6bf65cdca2af753f336660093b832
meargteame <mearegteame99995555@gmail.com>
2023-12-04 16:47:50 +0300

    printf

--- test/main.c
+++ test/main.c
@@ -1,5 +1,6 @@
 #include <limits.h>
 #include <stdio.h>
+#include <stdint.h> 
 #include "main.h"
 
 /**
@@ -17,7 +18,12 @@ int main(void)
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
     ui = (unsigned int)INT_MAX + 1024;
-    addr = (void *)0x7ffe637541f0;
+
+    
+      
+   
+
+
     _printf("Length:[%d, %i]\n", len, len);
     printf("Length:[%d, %i]\n", len2, len2);
     _printf("Negative:[%d]\n", -762534);
@@ -39,7 +45,7 @@ int main(void)
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
     _printf("Unknown:[%r]\n");
-    //printf("Unknown:[%r]\n");
+     
     return (0);
 }
 
