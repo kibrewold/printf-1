40e7186d9c0e0f934e12037f6bfe9ced445f3a23
meargteame <mearegteame99995555@gmail.com>
2023-12-04 16:09:15 +0300

    printf

--- main/main.c
+++ main/main.c
@@ -1,4 +1,3 @@
-#include <limits.h>
 #include <stdio.h>
 #include "main.h"
 
@@ -17,7 +16,7 @@ int main(void)
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
     ui = (unsigned int)INT_MAX + 1024;
-    addr = (void *)(unsigned long long)0x7ffe637541f0;  // Cast the constant to unsigned long long type
+    addr = (void *)0x7ffe637541f0;
     _printf("Length:[%d, %i]\n", len, len);
     printf("Length:[%d, %i]\n", len2, len2);
     _printf("Negative:[%d]\n", -762534);
@@ -38,7 +37,7 @@ int main(void)
     len2 = printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
-    //_printf("Unknown:[%r]\n");
-    //printf("Unknown:[%r]\n");
+    _printf("Unknown:[%r]\n");
+    printf("Unknown:[%r]\n");
     return (0);
 }
